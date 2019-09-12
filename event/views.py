a6cbc71da4a6dad2a0bfe9bb85eb4ba8324ddfca
jmdc-dkanazawa <dkanazawa@jmdc.co.jp>
2019-09-13 00:52:35 +0900

    Model change & ResultNew change

--- event/views.py
+++ event/views.py
@@ -1,8 +1,10 @@
 import math
+import pandas as pd
+import numpy as np
 from django.shortcuts import redirect, resolve_url
 from django.utils import timezone
 from django.views import generic
-from .models import Event, Player, Result
+from .models import Event, Player, Game, Result
 from .forms import EventForm, PlayerForm, ResultForm
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 
@@ -60,44 +62,26 @@ class ResultNew(generic.FormView):
         return kwargs
 
     def form_valid(self, form):
-        rank_p1 = 1
-        rank_p2 = 2
-        rank_p3 = 3
-        rank_p4 = 4
-        pt_p1 = math.floor(form.cleaned_data['point_p1']/1000)
-        pt_p2 = math.floor(form.cleaned_data['point_p2']/1000)
-        pt_p3 = math.floor(form.cleaned_data['point_p3']/1000)
-        pt_p4 = math.floor(form.cleaned_data['point_p4']/1000)
-        pt_uma_p1 = 30
-        pt_uma_p2 = 10
-        pt_uma_p3 = -10
-        pt_uma_p4 = -30
-        Result.objects.create(event=self.event,
-                              player=form.cleaned_data['p1'],
-                              point=form.cleaned_data['point_p1'],
-                              rank=rank_p1,
-                              pt=pt_p1,
-                              pt_uma=pt_uma_p1
-                              )
-        Result.objects.create(event=self.event,
-                              player=form.cleaned_data['p2'],
-                              point=form.cleaned_data['point_p2'],
-                              rank=rank_p2,
-                              pt=pt_p2,
-                              pt_uma=pt_uma_p2
-                              )
-        Result.objects.create(event=self.event,
-                              player=form.cleaned_data['p3'],
-                              point=form.cleaned_data['point_p3'],
-                              rank=rank_p3,
-                              pt=pt_p3,
-                              pt_uma=pt_uma_p3
-                              )
-        Result.objects.create(event=self.event,
-                              player=form.cleaned_data['p4'],
-                              point=form.cleaned_data['point_p4'],
-                              rank=rank_p4,
-                              pt=pt_p4,
-                              pt_uma=pt_uma_p4
-                              )
+        df = pd.DataFrame({'player': [form.cleaned_data['p1'],
+                                      form.cleaned_data['p2'],
+                                      form.cleaned_data['p3'],
+                                      form.cleaned_data['p4']],
+                           'point': [form.cleaned_data['point_p1'],
+                                     form.cleaned_data['point_p2'],
+                                     form.cleaned_data['point_p3'],
+                                     form.cleaned_data['point_p4']]
+                            })
+        df = df.reset_index().sort_values(by=["point", "index"], ascending=[False, True])
+        df = df.assign(rank=[1,2,3,4],
+                       pt=np.floor(df["point"]/1000)-30,
+                       pt_uma=[30,10,-10,-30])
+        game = Game.objects.create(event=self.event, uma_pattern=1)
+        for index, item in df.iterrows():
+            Result.objects.create(game=game,
+                                  player=item['player'],
+                                  point=item['point'],
+                                  rank=item['rank'],
+                                  pt=item['pt'],
+                                  pt_uma=item['pt_uma']
+                                  )
         return redirect('event:event_detail', pk=self.kwargs['pk'])
